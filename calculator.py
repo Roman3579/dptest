4ffa9f86dfd2be697579d4cf50ac278c53c051ae
RomanGlvac <60424242+Roman3579@users.noreply.github.com>
2022-11-13 17:28:46 +0100

    add student solution

---
+++ calculator.py
@@ -0,0 +1,16 @@
+
+class Calculator:
+
+    def add(self, first, second):
+        return first + second
+
+    def subtract(self, first, second):
+        return first - second
+
+    def multiply(self, first, second):
+        return first * second
+
+    def divide(self, first, second):
+        if second == 0:
+            raise ZeroDivisionError
+        return first / second
