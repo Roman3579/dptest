8569bf0d587fa9208355109cfc46151566cd6818
RomanGlvac <romanglvac@gmail.com>
2022-11-13 09:38:25 +0100

    add student solution

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,10 @@
+public class MainClass {
+
+    public static void main(String[] args) {
+//        TODO implementation
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+
+    }
+}
