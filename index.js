44853b91d2f8db32cd5282de445f4608b7cc15e0
RomanGlvac <60424242+Roman3579@users.noreply.github.com>
2022-12-06 17:22:43 +0100

    add student solution

---
+++ index.js
@@ -0,0 +1,10 @@
+const add = (first, second) => {
+  return first + second;
+};
+
+const subtract = (first, second) => {
+  return first - second;
+};
+
+module.exports.add = add;
+module.exports.subtract = subtract;
