51aa11d2c717659b561288433a226046fa359f2f
Roman3579 <romanglvac@gmail.com>
2023-04-30 18:38:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+const _ = require('lodash')
 
 const add = (first, second) => {
   return first + second;
@@ -12,9 +12,6 @@ const create_range = (start, end) => {
   return _.range(start, end);
 }
 
-const _add = add;
-export { _add as add };
-const _subtract = subtract;
-export { _subtract as subtract };
-const _create_range = create_range;
-export { _create_range as create_range };
+module.exports.add = add;
+module.exports.subtract = subtract;
+module.exports.create_range = create_range;
